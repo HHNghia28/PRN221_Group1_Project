9a5b1cccec96b84aa508d3e35e57db36b7deaae1
hieuttn <hieuttn.work@gmail.com>
2023-09-22 09:43:53 +0700

    Cashier

---
+++ PRN221_Group1_Project/G1Food_Cashier/Pages/CashierAtStore.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace G1Food_Cashier.Pages.Shared
+{
+    public class CashierAtSroreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
