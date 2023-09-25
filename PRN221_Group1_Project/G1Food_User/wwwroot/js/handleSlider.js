8a62a8efbb9e10d2d523e362566f4b3cc07d268e
KCodeGuy <106859158+KCodeGuy@users.noreply.github.com>
2023-09-25 21:06:27 +0700

    KhoaTD fix User page

---
+++ PRN221_Group1_Project/G1Food_User/wwwroot/js/handleSlider.js
@@ -0,0 +1,56 @@
+$('#slider-header').owlCarousel({
+    loop:true,
+    items: 1,
+    margin:10,
+    dots:true,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:true,
+});
+
+$('#slider-voucher').owlCarousel({
+    loop:true,
+    items: 4,
+    dotsEach: 1,
+    margin:10,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            loop:true,
+        }
+    }
+})
+
+$('#slider-category').owlCarousel({
+    loop:true,
+    dotsEach: 1,
+    items: 1,
+    margin:1,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2,
+        },
+        600:{
+            items:4,
+        },
+        1000:{
+            items:6,
+        }
+    }
+})
