be3b8a4fd31a502c69519ab17e4cd7240315d3d1
KCodeGuy <106859158+KCodeGuy@users.noreply.github.com>
2023-09-25 22:32:50 +0700

    Fix link page css and show order detail form

--- PRN221_Group1_Project/G1Food_User/wwwroot/js/main.js
+++ PRN221_Group1_Project/G1Food_User/wwwroot/js/main.js
@@ -1,7 +1,8 @@
 // Set active navbar tab
 const navItems = document.querySelectorAll('.header-list li');
 navItems.forEach(item => {
-    item.addEventListener('click', () => {
+    item.addEventListener('click', (e) => {
+        //e.preventDefault();
         const currentActiveTab = document.querySelector('.header-list li.active');
         console.log(currentActiveTab);
         currentActiveTab.classList.remove('active');
@@ -9,41 +10,6 @@ navItems.forEach(item => {
     })
 })
 
-// Show hide menu 
-const overplay = document.querySelector('.overplay');
-const orderedList = document.querySelectorAll('.order-list tr');
-const orderDetailContainer = document.querySelector('.oderDetail-container');
-const btnCloseOrderDetail = document.querySelector('.btn-close-order-detail');
-console.log(overplay);
-orderedList.forEach((order, index) => {
-    order.addEventListener('click', () => {
-        overplay.classList.add('d-block');
-        orderDetailContainer.classList.add('d-block');
-    })
-})
-
-btnCloseOrderDetail.addEventListener('click', () => {
-    overplay.classList.remove('d-block');
-    orderDetailContainer.classList.remove('d-block');
-})
-
-// Handle inscrease quantity in product detail
-const increaseQuantity = (button)  => {
-    const parentlEement = button.parentNode ;
-    const quantityInput = parentlEement.querySelector('#quantity-input');
-    console.log(quantityInput);
-    let quantity = parseInt(quantityInput.value, 10);
-    quantityInput.value = quantity + 1;
-  }
-  
-const decreaseQuantity = (button) => {
-    const parentlEement = button.parentNode;
-    const quantityInput = parentlEement.querySelector('#quantity-input');
-    let quantity = parseInt(quantityInput.value, 10);
-    if (quantity > 1) {
-      quantityInput.value = quantity - 1;
-    }
-}
 //   Handle show form input comment
 // const inputComment = document.querySelector('.comment-wrapper.reply input');
 // const buttonReplyComment = document.querySelector('.comment-wrapper.reply button');
