efceae17847f8b8c9da0bd6045a148c082b60170
Nghia Huynh <nghiahh.work@gmail.com>
2023-10-03 20:05:49 +0700

    Update Index.cshtml.cs

--- PRN221_Group1_Project/G1Food_User/Pages/Index.cshtml.cs
+++ PRN221_Group1_Project/G1Food_User/Pages/Index.cshtml.cs
@@ -1,51 +1,65 @@
 ﻿using G1FOODLibrary.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace G1Food_User.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly HttpClient _client = null;
-        private string _scheduleApiUrl;
+        private readonly HttpClient _client;
+        private readonly string _scheduleApiUrl;
 
-        public IEnumerable<MenuResponse> Products { get; set; }
+        public IEnumerable<MenuResponse> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
             _client = new HttpClient();
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             _client.DefaultRequestHeaders.Accept.Add(contentType);
-            _scheduleApiUrl = GetAPIEndpointSchedule();
-        }
-
-        private string GetAPIEndpointSchedule()
-        {
-            IConfiguration config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true, true).Build();
-            var strConn = config["APIEndpoint:Schedule"];
-            return strConn;
+            _scheduleApiUrl = configuration.GetValue<string>("APIEndpoint:Schedule");
         }
 
         public async Task OnGet()
         {
-            HttpResponseMessage response = await _client.GetAsync(_scheduleApiUrl + "getMenuNow");
-            string stringData = await response.Content.ReadAsStringAsync();
-            var options = new JsonSerializerOptions
+            try
             {
-                PropertyNameCaseInsensitive = true
-            };
+                HttpResponseMessage response = await _client.GetAsync($"{_scheduleApiUrl}getMenuNow");
+                response.EnsureSuccessStatusCode();
 
-            APIResponse apiResponse = JsonSerializer.Deserialize<APIResponse>(stringData, options);
+                string stringData = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
 
-            if (apiResponse.Success)
+                APIResponse apiResponse = JsonSerializer.Deserialize<APIResponse>(stringData, options);
+
+                if (apiResponse.Success)
+                {
+                    Products = JsonSerializer.Deserialize<List<MenuResponse>>(apiResponse.Data.ToString(), options);
+                }
+                else
+                {
+                    _logger.LogError($"API call failed with message: {apiResponse.Message}");
+                }
+            }
+            catch (HttpRequestException ex)
             {
-                Products = JsonSerializer.Deserialize<List<MenuResponse>>(apiResponse.Data.ToString(), options);
+                _logger.LogError($"HTTP request failed with error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred: {ex.Message}");
             }
         }
     }
